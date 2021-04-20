a7f0086868608b88e9de4574137b059233720c10
izotych <74318989+izotych@users.noreply.github.com>
2021-04-20 18:05:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace Kata_sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5] { 2, 1, 3, 4, 5};
+            
+            Sort(array);
+            Read(array);
+        }
+
+        public static void Sort(int[] mass)
+        {
+            int Sas = 0;
+            int temp;
+            bool swap;
+            for (int j = 0; j < mass.Length; j++)
+            {
+                swap = false;
+                // При каждом обмене, мы должны меньше раз пробегать по массиву
+                for (int i = 0; i < mass.Length - 1 - j; i++)
+                {
+                    
+                    if (mass[i] > mass[i + 1])
+                    {
+                        temp = mass[i];
+                        mass[i] = mass[i + 1];
+                        mass[i + 1] = temp;
+                        swap = true;
+                    }
+                    Sas += 1;
+                   
+                }
+                if (!swap)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(Sas + " раз прошли по членам массива");
+        }
+        public static void Read(int[] mass)
+        {
+            foreach(int elem in mass)
+            {
+                Console.WriteLine(elem);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
